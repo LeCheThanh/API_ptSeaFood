19766d738dc5d2d9c4f6b3842ea49d87ddafe635
CheThanh <chethanh3112@gmail.com>
2023-10-16 17:17:06 +0700

    feat: config Jwt

---
+++ src/main/java/seaFood/PTseafood/service/JwtService.java
@@ -0,0 +1,36 @@
+package seaFood.PTseafood.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+import seaFood.PTseafood.entity.User;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Service
+public class JwtService {
+    private static final String Secret_key="123";
+
+    public String generateToken(User user, Collection<SimpleGrantedAuthority> authorities){
+        //generate token use Jwt depen
+        Algorithm algorithm = Algorithm.HMAC256(Secret_key.getBytes());
+        return JWT.create()
+                .withSubject(user.getEmail())
+                .withExpiresAt(new Date(System.currentTimeMillis() +50 *60 * 1000))
+                .withClaim("roles",authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+    }
+    public String generateRefreshToken(User user, Collection<SimpleGrantedAuthority> authorities){
+        //generate token use Jwt depen
+        Algorithm algorithm = Algorithm.HMAC256(Secret_key.getBytes());
+        return JWT.create()
+                .withSubject(user.getEmail())
+                .withExpiresAt(new Date(System.currentTimeMillis() +70 *60 * 1000))
+                .sign(algorithm);
+    }
+}
