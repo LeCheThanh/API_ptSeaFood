b05b73081beda8720b804fe0d5e2b7b8c17e75f0
CheThanh <chethanh3112@gmail.com>
2023-10-28 14:34:57 +0700

    feat : enum

--- src/main/java/seaFood/PTseafood/common/Enum.java
+++ src/main/java/seaFood/PTseafood/common/Enum.java
@@ -4,4 +4,26 @@ public class Enum {
     public enum Rank {
         unRank, Silver, Gold, Platinum
     }
+    public enum Role{
+        User("User"),
+
+        Admin("Admin"),
+
+        Manager("Manager");
+
+        private String name;
+
+        Role(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public enum Provider{
+        LOCAL,
+        GOOGLE
+    }
 }
