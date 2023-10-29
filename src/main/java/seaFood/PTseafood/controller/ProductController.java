6258248128a789dfbf3f021010a2d3b73aeb41cc
CheThanh <chethanh3112@gmail.com>
2023-10-29 15:20:27 +0700

    fix: searchProduct

--- src/main/java/seaFood/PTseafood/controller/ProductController.java
+++ src/main/java/seaFood/PTseafood/controller/ProductController.java
@@ -1,5 +1,7 @@
 package seaFood.PTseafood.controller;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import seaFood.PTseafood.service.ProductService;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
@@ -65,10 +68,43 @@ public class ProductController {
             reponse.put("deleted",Boolean.TRUE);
             return ResponseEntity.ok(reponse);
     }
-    @GetMapping("/search")
+
     public List<Product> searchProducts(@RequestParam String keyword) {
         List<Product> products = productService.findByNameContaining(keyword);
         return products;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> search(@RequestParam("keyword") String keyword)  { // dùng chấm hỏi chỗ <?> để cho
+        // kiểu đại diện vì phải trả về message và list book!
+        // Xử lý tìm kiếm và trả về kết quả dưới dạng List<Book>
+        List<Product> searchResults = performSearch(keyword);
+        if (searchResults.isEmpty()) {
+            JSONObject responseJson = new JSONObject();
+            responseJson.put("message", "Không tìm thấy sản phẩm cần search!");
+
+            return ResponseEntity.badRequest().body(responseJson.toString());
+        }
+        JSONArray jsonArr = new JSONArray();
+        for (Product product : searchResults) {
+            JSONObject productJson = new JSONObject();
+            productJson.put("name", product.getName());
+            productJson.put("description", product.getDescription());
+            productJson.put("image", product.getImage());
+            productJson.put("slug", product.getSlug());
+            jsonArr.add(productJson);
+        }
+        return ResponseEntity.ok(jsonArr.toString());
+    }
+
+    public List<Product> performSearch(String keyword) {
+        // Thực hiện tìm kiếm trong danh sách đối tượng
+        List<Product> searchResults = productService.getAll().stream()
+                .filter(m -> m.getName().toUpperCase().contains(keyword.toUpperCase())
+                        || m.getDescription().toUpperCase().contains(keyword.toUpperCase()))
+                .collect(Collectors.toList());
+
+        return searchResults;
     }
 
 }
