9a7cc113f5bb366219d47ea2e7d48fe1f43c72df
CheThanh <chethanh3112@gmail.com>
2023-11-01 16:36:43 +0700

    feat: create some entities

--- src/main/java/seaFood/PTseafood/entity/Order.java
+++ src/main/java/seaFood/PTseafood/entity/Order.java
@@ -1,4 +1,57 @@
 package seaFood.PTseafood.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name="orders")
 public class Order {
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Column(name = "receiver_name", nullable = false)
+    private String receiverName;
+
+    @Column(name = "receiver_phone", nullable = false)
+    private String receiverPhone;
+
+    @Column(name = "receiver_address", nullable = false)
+    private String receiverAddress;
+
+    @Column(name = "receiver_email", nullable = false)
+    private String receiverEmail;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @Column(name = "total_price")
+    private Double totalPrice;
+
+    @Column(name = "final_price")
+    private Double finalPrice;
+
+    @Column(name = "discount_price")
+    private Double discountPrice;
+
+    @Column(name = "payment_method", nullable = false)
+    private String paymentMethod;
+
+    @Column(name = "payment_status", nullable = false)
+    private String paymentStatus;
+
+    @Column(name = "note")
+    private Double note;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderState> orderStates = new ArrayList<>();
+ }
--- src/main/java/seaFood/PTseafood/entity/OrderState.java
+++ src/main/java/seaFood/PTseafood/entity/OrderState.java
@@ -1,4 +1,27 @@
 package seaFood.PTseafood.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name="order_states")
 public class OrderState {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "state", nullable = false)
+    private String state;
+
+    @Column(name = "created_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Order order;
 }
