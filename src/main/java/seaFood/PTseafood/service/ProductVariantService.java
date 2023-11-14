22832621fa90cfb1218c0b4e918f46d3432ed0ab
CheThanh <chethanh3112@gmail.com>
2023-11-14 20:37:11 +0700

    refactor: vnpay

--- src/main/java/seaFood/PTseafood/service/ProductVariantService.java
+++ src/main/java/seaFood/PTseafood/service/ProductVariantService.java
@@ -2,16 +2,22 @@ package seaFood.PTseafood.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import seaFood.PTseafood.entity.Product;
 import seaFood.PTseafood.entity.ProductVariant;
+import seaFood.PTseafood.repository.IProductRepository;
 import seaFood.PTseafood.repository.IProductVariantRepository;
 import seaFood.PTseafood.utils.SlugUtil;
+import seaFood.PTseafood.dto.ProductStatistic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductVariantService {
     @Autowired
     private IProductVariantRepository productVariantRepository;
+    @Autowired
+    private IProductRepository productRepository;
     public ProductVariant getById(Long id) {
         return productVariantRepository.findById(id).orElse(null);
     }
@@ -28,6 +34,21 @@ public class ProductVariantService {
     }
     public List<ProductVariant> findOutOfStockProducts() {
         return productVariantRepository.findByStock(0);
+    }
+    ///Thong ke theo sp da ban
+    public List<ProductStatistic> getProductStatistics() {
+        List<Product> products = productRepository.findAll();
+        List<ProductStatistic> productStatistics = new ArrayList<>();
+
+        for (Product product : products) {
+            List<ProductVariant> variants = productVariantRepository.findByProduct(product);
+            int totalSoldQuantity = variants.stream().mapToInt(ProductVariant::getSoldQuantity).sum();
+            double totalRevenue = variants.stream().mapToDouble(v -> v.getSoldQuantity() * v.getPrice()).sum();
+            ProductStatistic productStatistic = new ProductStatistic(product, totalSoldQuantity, totalRevenue);
+            productStatistics.add(productStatistic);
+        }
+
+        return productStatistics;
     }
 
 }
