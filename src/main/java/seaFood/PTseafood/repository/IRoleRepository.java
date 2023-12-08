c7abbf0621b43942c770ad66f90470cb6afb568f
CheThanh <chethanh3112@gmail.com>
2023-12-08 18:21:50 +0700

    feat: getRoles

--- src/main/java/seaFood/PTseafood/repository/IRoleRepository.java
+++ src/main/java/seaFood/PTseafood/repository/IRoleRepository.java
@@ -3,8 +3,11 @@ package seaFood.PTseafood.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import seaFood.PTseafood.entity.Role;
 
+import java.util.List;
+
 public interface IRoleRepository extends JpaRepository<Role,Long> {
     Role findByName(String role);
 //    @Query("SELECT r.id FROM Role r WHERE r.name = ?1")
 //    Long getRoleIdByName(String roleName);
+    List<Role> findRolesByUserId(Long id);
 }
